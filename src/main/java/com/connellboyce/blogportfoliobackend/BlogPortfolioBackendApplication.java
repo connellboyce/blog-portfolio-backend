3cdadafc454d60ce0c1592fe1a046c6aff254497
Connell Boyce <connell.p.boyce@gmail.com>
2021-06-24 15:23:25 -0400

    Initializing Spring Boot files

---
+++ src/main/java/com/connellboyce/blogportfoliobackend/BlogPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.connellboyce.blogportfoliobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPortfolioBackendApplication.class, args);
+	}
+
+}
